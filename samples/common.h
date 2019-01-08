85920bb461b87893738d213d7c2393b5dc1d2ba4
Liu Yiqun <liuyiqun01@baidu.com>
2019-01-08 12:19:40 +0800

    Add unittest for some fluid models.

---
+++ samples/common.h
@@ -0,0 +1,335 @@
+/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include <glog/logging.h>
+#include <fstream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <dirent.h>
+
+#include "paddle/fluid/inference/tests/api/tester_helper.h"
+
+DEFINE_bool(use_gpu, true, "Whether use gpu.");
+DEFINE_bool(use_tensorrt, true, "Test the performance of TensorRT engine.");
+DEFINE_string(prog_filename, "", "Name of model file.");
+DEFINE_string(param_filename, "", "Name of parameters file.");
+DEFINE_string(image_dir, "", "Path of input data.");
+DEFINE_string(image_dims, "", "Dims of input data.");
+
+namespace paddle {
+namespace inference {
+
+void SetInputs(std::vector<paddle::PaddleTensor> &input_tensors, std::string &input_path, std::string &input_dims);
+
+std::vector<int> ParseDims(std::string dims_str) {
+  std::vector<int> dims;
+  std::string token;
+  std::istringstream token_stream(dims_str);
+  while (std::getline(token_stream, token, 'x')) {
+    dims.push_back(std::stoi(token));
+  }
+  return dims;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& value) {
+  os << "{";
+  if (value.size() > 0) {
+    os << value[0];
+  }
+  for (size_t i = 1; i < value.size(); ++i) {
+    os << ", " << value[i];
+  }
+  os << "}";
+  return os;
+}
+
+template <typename T>
+void SetupTensor(const std::string filename, paddle::PaddleTensor& tensor,
+                 std::vector<int>* shape, T mean = 0) {
+  std::ifstream is(filename);
+
+  size_t size = 1;
+  for (size_t i = 0; i < shape->size(); ++i) {
+    if (shape->at(i) <= 0) {
+      int64_t l = 0;
+      is >> l;
+      shape->at(i) = l;
+    }
+    size *= shape->at(i);
+  }
+  LOG(INFO) << "shape: " << *shape;
+
+  std::vector<T> data;
+  for (size_t i = 0; i < size; ++i) {
+    T value;
+    is >> value;
+    data.push_back(static_cast<T>(value - mean));
+  }
+  is.close();
+
+  tensor.shape = *shape;
+  tensor.data.Resize(sizeof(T) * size);
+  std::copy(data.begin(), data.end(), static_cast<T*>(tensor.data.data()));
+}
+
+template <typename T>
+void SetupTensor(paddle::PaddleTensor& tensor, std::vector<int> shape, T lower,
+                 T upper) {
+  size_t size = 1;
+  for (size_t i = 0; i < shape.size(); ++i) {
+    size *= shape[i];
+  }
+  LOG(INFO) << "shape: " << shape;
+
+  static unsigned int seed = 100;
+  std::mt19937 rng(seed++);
+  std::uniform_real_distribution<double> uniform_dist(0, 1);
+
+  std::vector<T> data;
+  for (size_t i = 0; i < size; ++i) {
+    data.push_back(static_cast<T>(uniform_dist(rng) * (upper - lower) + lower));
+  }
+
+  tensor.shape = shape;
+  tensor.data.Resize(sizeof(T) * size);
+  std::copy(data.begin(), data.end(), static_cast<T*>(tensor.data.data()));
+}
+
+template <typename ConfigType>
+void SetConfig(ConfigType* config, std::string model_dir, bool use_gpu,
+               bool use_tensorrt = false, int batch_size = -1) {
+  if (!FLAGS_prog_filename.empty() && !FLAGS_param_filename.empty()) {
+    config->prog_file = model_dir + "/" + FLAGS_prog_filename;
+    config->param_file = model_dir + "/" + FLAGS_param_filename;
+  } else {
+    config->model_dir = model_dir;
+  }
+  if (use_gpu) {
+    config->use_gpu = true;
+    config->device = 0;
+    config->fraction_of_gpu_memory = 0.15;
+  }
+}
+
+template <>
+void SetConfig<contrib::AnalysisConfig>(contrib::AnalysisConfig* config,
+                                        std::string model_dir, bool use_gpu,
+                                        bool use_tensorrt, int batch_size) {
+  if (!FLAGS_prog_filename.empty() && !FLAGS_param_filename.empty()) {
+    config->SetModel(model_dir + "/" + FLAGS_prog_filename,
+                     model_dir + "/" + FLAGS_param_filename);
+  } else {
+    config->SetModel(model_dir);
+  }
+  if (use_gpu) {
+    config->EnableUseGpu(100, 0);
+    if (use_tensorrt) {
+      config->EnableTensorRtEngine(1 << 10, batch_size);
+      config->pass_builder()->DeletePass("conv_bn_fuse_pass");
+      config->pass_builder()->DeletePass("fc_fuse_pass");
+      config->pass_builder()->TurnOnDebug();
+    } else {
+      config->SwitchIrOptim();
+    }
+  }
+}
+
+int GenerateInputList(std::vector<std::string>* input_list, std::string& input_dir) {
+  DIR *dir = NULL;
+  if ((dir = opendir(input_dir.c_str())) == NULL) {
+    std::cerr << "Open image dir failed. Dir: " << input_dir << std::endl;
+    return -1;
+  }
+
+  input_list->clear();
+  struct dirent* ptr = NULL;
+  while ((ptr = readdir(dir)) != NULL) {
+    if (ptr->d_name[0] == '.') {
+      continue;
+    }
+    std::string filename = std::string(ptr->d_name);
+    if (filename.length() < 5){
+      std::cerr << "Wrong image file format: " << filename << std::endl;
+      continue;
+    } else {
+      std::string suffix = filename.substr(filename.length() - 4, 4);
+      if (suffix.compare(".txt") != 0){
+        std::cerr << "Wrong image file format: " << filename << std::endl;
+        continue;
+      }
+    }
+    input_list->push_back(filename);
+  }
+  closedir(dir);
+  return 0;
+}
+
+void TestImpl(const PaddlePredictor::Config *config, std::vector<paddle::PaddleTensor> *outputs, bool use_analysis = true) {
+  int batch_size = FLAGS_batch_size;
+  int num_times = FLAGS_repeat;
+  auto predictor = CreateTestPredictor(config, use_analysis);
+
+  if (FLAGS_profile) {
+    paddle::platform::ResetProfiler();
+  }
+
+  std::vector<std::string> input_list;
+  if (GenerateInputList(&input_list, FLAGS_image_dir)) {
+    LOG(WARNING) << "Get no inputs in image_dir (" << FLAGS_image_dir << "), use fake inputs instead.";
+    input_list.push_back("dummpy");
+  }
+
+  double total_latency = 0.0f;
+  for (size_t i = 0; i < input_list.size(); ++i) {
+    std::vector<paddle::PaddleTensor> inputs;
+    outputs->clear();
+
+    if (input_list[i] != "dummpy") {
+      std::string input_path = FLAGS_image_dir + "/" + input_list[i];
+      SetInputs(inputs, input_path, FLAGS_image_dims);
+    } else {
+      std::string input_path = "";
+      SetInputs(inputs, input_path, FLAGS_image_dims);
+    }
+
+    if (input_list.size() <= 1) {
+      // warmup run
+      LOG(INFO) << "Warm up run...";
+      {
+        Timer warmup_timer;
+        warmup_timer.tic();
+        predictor->Run(inputs, outputs, batch_size);
+        PrintTime(batch_size, 1, 1, 0, warmup_timer.toc(), 1);
+        if (FLAGS_profile) {
+          paddle::platform::ResetProfiler();
+        }
+      }
+
+      LOG(INFO) << "Run " << num_times << " times...";
+    }
+          
+    {
+      Timer run_timer;
+      run_timer.tic();
+      for (int r = 0; r < num_times; r++) {
+        predictor->Run(inputs, outputs, batch_size);
+      }
+      double latency = run_timer.toc() / num_times;
+      total_latency += latency;
+    }
+  }
+  PrintTime(batch_size, num_times, 1, 0, total_latency, input_list.size());
+}
+
+template <typename T>
+std::string DataToString1D(T* data, size_t length, size_t print_elements) {
+  if (length < 1) {
+    LOG(FATAL) << "Invalid data.";
+  }
+
+  std::ostringstream os;
+  os << "[" << data[0];
+  if (length <= 2 * print_elements) {
+    for (size_t i = 1; i < length; ++i) {
+      os << ", " << data[i];
+    }
+  } else {
+    for (size_t i = 1; i < print_elements; ++i) {
+      os << ", " << data[i];
+    }
+    os << " ... " << data[length - print_elements];
+    for (size_t i = length - print_elements + 1; i < length; ++i) {
+      os << ", " << data[i];
+    }
+  }
+  os << "]";
+  return os.str();
+}
+
+template <typename T>
+std::string PrintData(T* data, const std::vector<int>& shape,
+                      size_t print_elements) {
+  size_t num_dims = shape.size();
+  if (num_dims <= 0) {
+    return "";
+  }
+
+  size_t num_cols = shape[num_dims - 1];
+
+  if (num_dims == 1) {
+    return DataToString1D(data, num_cols, print_elements);
+  } else {
+    size_t num_rows = 1;
+    std::string spaces = "";
+    std::ostringstream os;
+    for (size_t i = 0; i < num_dims - 1; ++i) {
+      os << "[";
+      spaces += " ";
+      num_rows *= shape[i];
+    }
+
+    os << DataToString1D(data, num_cols, print_elements);
+    if (num_rows <= 2 * print_elements) {
+      for (size_t i = 1; i < num_rows; ++i) {
+        os << "\n"
+           << spaces
+           << DataToString1D(data + i * num_cols, num_cols, print_elements);
+      }
+    } else {
+      for (size_t i = 1; i < print_elements; ++i) {
+        os << "\n"
+           << spaces
+           << DataToString1D(data + i * num_cols, num_cols, print_elements);
+      }
+      os << "\n" << spaces << "...\n" << spaces << "...\n" << spaces << "...";
+      for (size_t i = num_rows - print_elements; i < num_rows; ++i) {
+        os << "\n"
+           << spaces
+           << DataToString1D(data + i * num_cols, num_cols, print_elements);
+      }
+    }
+
+    for (size_t i = 0; i < num_dims - 1; ++i) {
+      os << "]";
+    }
+    return os.str();
+  }
+}
+
+void PrintTensor(const PaddleTensor& tensor, size_t print_elements = 0) {
+  LOG(INFO) << "name: " << tensor.name;
+  LOG(INFO) << "shape: " << tensor.shape;
+  LOG(INFO) << "lod: " << tensor.lod;
+  if (tensor.dtype == PaddleDType::FLOAT32) {
+    LOG(INFO) << "dtype: PaddleDType::FLOAT32";
+    LOG(INFO) << "data:\n"
+              << PrintData<float>(static_cast<float*>(tensor.data.data()),
+                                  tensor.shape, print_elements);
+  } else if (tensor.dtype == PaddleDType::INT64) {
+    LOG(INFO) << "datype: PaddleDType::INT64";
+    LOG(INFO) << "data:\n"
+              << PrintData<int64_t>(static_cast<int64_t*>(tensor.data.data()),
+                                    tensor.shape, print_elements);
+  } else {
+    LOG(FATAL) << "Unsupported dtype.";
+  }
+}
+
+}  // namespace inference
+}  // namespace paddle
---
+++ samples/ocr_plate_tester.cc
@@ -0,0 +1,211 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "common.h"
+
+namespace paddle {
+namespace inference {
+
+void ConvertOutput(const std::vector<paddle::PaddleTensor> &tensors) {
+  std::vector<std::vector<float>> data_alls;
+  std::vector<std::vector<int>> shape_alls;
+
+  // use reference to avoid double free
+  for (auto &t : tensors) {
+    shape_alls.push_back(t.shape);
+    const size_t num_elements = t.data.length() / sizeof(float);
+    float *t_data = static_cast<float *>(t.data.data());
+    std::vector<float> data(num_elements, 0);
+    std::copy(t_data, t_data + num_elements, data.data());
+    data_alls.push_back(data);
+  }
+
+  // std::string plate_str = "";
+  // for (float k : data_alls[0]) {
+  //   std::cerr << "text\t" << k << std::endl;
+  //   if (k == 0 || k == 1 || k == 2) {
+  //     continue;
+  //   }
+  //   // plate_str += table_dict[k];
+  // }
+
+  // for (float k : data_alls[1]) {
+  //   std::cerr << "scores\t" << k << std::endl;
+  // }
+}
+
+void SetInputs(std::vector<paddle::PaddleTensor> &input_tensors, std::string &image_path, std::string &image_dims) {
+  //
+  // image tensor -> pixel
+  //
+  paddle::PaddleTensor image_tensor;
+
+  int batch_size = 1;
+  int channel = 1;
+  int height = 48;
+  int width = 512;
+  if (!image_dims.empty()) {
+    std::vector<int> image_shape = ParseDims(image_dims);
+    size_t length = image_shape.size();
+    if (length >= 4) {
+      batch_size = image_shape[length - 4];
+    }
+    if (length >= 3) {
+      channel = image_shape[length - 3];
+    }
+    if (length >= 2) {
+      height = image_shape[length - 2];
+    }
+    if (length >= 1) {
+      width = image_shape[length - 1];
+    }
+  }
+
+  image_tensor.name = "pixel";
+  image_tensor.dtype = paddle::PaddleDType::FLOAT32;
+  std::vector<int> image_shape = {batch_size, channel, height, width};
+  if (image_path.empty()) {
+    SetupTensor<float>(image_tensor, image_shape,
+                       static_cast<float>(-1), static_cast<float>(1));
+  } else {
+    LOG(INFO) << "image_path: " << image_path;
+    SetupTensor<float>(image_path, image_tensor, &image_shape, 127.5);
+  }
+
+  //
+  // init_ids_tensor -> init_ids
+  //
+  paddle::PaddleTensor init_ids_tensor;
+
+  std::vector<int> ids_shape;
+  ids_shape.push_back(batch_size);
+  ids_shape.push_back(1);
+
+  std::vector<int64_t> init_ids;
+  init_ids.push_back(0);
+
+  init_ids_tensor.name = "init_ids";
+  init_ids_tensor.shape = ids_shape;
+  init_ids_tensor.dtype = paddle::PaddleDType::INT64;
+  init_ids_tensor.data.Resize(sizeof(int64_t) * 1);
+  std::copy(init_ids.begin(), init_ids.end(),
+            static_cast<int64_t *>(init_ids_tensor.data.data()));
+
+  std::vector<size_t> lod_1 = {0, 1};
+  std::vector<size_t> lod_2 = {0, 1};
+  std::vector<std::vector<size_t>> lod = {{0, 1}, {0, 1}};
+  init_ids_tensor.lod = lod;
+
+  //
+  // init scores
+  //
+  paddle::PaddleTensor init_scores_tensor;
+
+  std::vector<int> scores_shape;
+  scores_shape.push_back(1);
+  scores_shape.push_back(1);
+
+  std::vector<float> init_scores;
+  init_scores.push_back(1.0);
+
+  init_scores_tensor.name = "init_scores";
+  init_scores_tensor.shape = scores_shape;
+  init_scores_tensor.dtype = paddle::PaddleDType::FLOAT32;
+  init_scores_tensor.data.Resize(sizeof(float) * 1);
+  std::copy(init_scores.begin(), init_scores.end(),
+            static_cast<float *>(init_scores_tensor.data.data()));
+  init_scores_tensor.lod = lod;
+
+  // input_tensors
+  input_tensors.push_back(image_tensor);
+  input_tensors.push_back(init_ids_tensor);
+  input_tensors.push_back(init_scores_tensor);
+
+#if 0
+  //
+  // position encoding
+  //
+  paddle::PaddleTensor position_encoding_tensor;
+
+  std::vector<int> position_encoding_shape;
+  position_encoding_shape.push_back(1);
+  position_encoding_shape.push_back(33);
+  position_encoding_shape.push_back(10);
+  position_encoding_shape.push_back(23);
+
+  std::vector<int> pos_data;
+  for (int i = 0; i < 10; i++) {
+    for (int row = 0; row < 10; row++) {
+      for (int col = 0; col < 23; col++) {
+        if (i == row) {
+          pos_data.push_back(1);
+        } else {
+          pos_data.push_back(0);
+        }
+      }
+    }
+  }
+  for (int i = 0; i < 23; i++) {
+    for (int row = 0; row < 10; row++) {
+      for (int col = 0; col < 23; col++) {
+        if (i == col) {
+          pos_data.push_back(1);
+        } else {
+          pos_data.push_back(0);
+        }
+      }
+    }
+  }
+
+  position_encoding_tensor.name = "position_encoding";
+  position_encoding_tensor.shape = position_encoding_shape;
+  position_encoding_tensor.dtype = paddle::PaddleDType::FLOAT32;
+  position_encoding_tensor.data.Resize(sizeof(float) * 33 * 10 * 23);
+  std::copy(pos_data.begin(), pos_data.end(),
+            static_cast<float *>(position_encoding_tensor.data.data()));
+  input_tensors.push_back(position_encoding_tensor);
+#endif
+}
+
+void profile(std::string model_dir, bool use_gpu, bool use_analysis, bool use_tensorrt) {
+  std::vector<paddle::PaddleTensor> outputs;
+
+  if (use_gpu && (use_analysis || use_tensorrt)) {
+    contrib::AnalysisConfig config;
+    SetConfig<contrib::AnalysisConfig>(&config, model_dir, use_gpu, use_tensorrt,
+                                       FLAGS_batch_size);
+    TestImpl(reinterpret_cast<PaddlePredictor::Config *>(&config), &outputs, true);
+  } else {
+    NativeConfig config;
+    SetConfig<NativeConfig>(&config, model_dir, use_gpu, false);
+    TestImpl(reinterpret_cast<PaddlePredictor::Config *>(&config), &outputs, false);
+  }
+
+  for (size_t i = 0; i < outputs.size(); ++i) {
+    LOG(INFO) << "<<< output: " << i << " >>>";
+    PrintTensor(outputs[i], 4);
+  }
+}
+
+TEST(attention_ocr, profile) {
+  std::string model_dir = FLAGS_infer_model;
+  profile(model_dir, FLAGS_use_gpu, FLAGS_use_analysis, FLAGS_use_tensorrt);
+}
+
+}  // namespace inference
+}  // namespace paddle
